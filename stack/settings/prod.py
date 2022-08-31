5fc91036cebf2a670ac240095c05f60ee61bc40d
Vinod Kumar <django2python3@gmail.com>
2022-08-31 13:36:11 +0530

    updated code

--- stack/settings/prod.py
+++ stack/settings/prod.py
@@ -10,3 +10,10 @@ DATABASES = {
         'PORT': config('DATABASE_PORT', default=5432)
     }
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
